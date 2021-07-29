7b284af483cde06c13b6c91bece9782e4a911709
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-07-29 21:54:19 +0700

    first commit

---
+++ src/redux/shop/shopActions.js
@@ -0,0 +1,33 @@
+import * as actionTypes from "./shopTypes";
+
+export const setCategories = (categories) => {
+	return {
+		type: actionTypes.SET_CATEGORIES,
+		payload: categories,
+	};
+};
+export const setProducts = (products) => {
+	return {
+		type: actionTypes.SET_PRODUCTS,
+		payload: products,
+	};
+};
+
+export const loadCurrentProduct = (item) => {
+	return {
+		type: actionTypes.LOAD_CURRENT_PRODUCT,
+		payload: item,
+	};
+};
+export const removeCurrentProduct = () => {
+	return {
+		type: actionTypes.REMOVE_CURRENT_PRODUCT,
+	};
+};
+
+export const showMessage = (show) => {
+	return {
+		type: actionTypes.SHOW_MESSAGE,
+		payload: show,
+	};
+};
